82a903fa281b857a6e66af9a72c03096521dbd9c
Grizius <daniel.ittel@gmail.com>
2024-03-24 21:38:56 +0100

    fonctionne pour un pret a taux zero

--- HomeLoan/HomeLoan.Tests/HomeLoanTests.cs
+++ HomeLoan/HomeLoan.Tests/HomeLoanTests.cs
@@ -13,9 +13,15 @@ public class HomeLoanTests
     }
 
     [Theory]
+
     [InlineData(0, 1, 0, 0)]
+
     [InlineData(1000, 1, 0, 1000)]
     [InlineData(10000, 1, 0, 10000)]
+
+    [InlineData(1000, 10, 0, 100)]
+    [InlineData(10000, 10, 0, 1000)]
+    [InlineData(10000, 100, 0, 100)]
     public void CaculateMonthlyPayment_ShouldWork(int borrowedAmount, int duration, decimal rate, int expectedResult)
     {
         var actualResult = HomeLoan.CaculateMonthlyPayment(borrowedAmount, duration, rate);
--- HomeLoan/HomeLoan/HomeLoan.cs
+++ HomeLoan/HomeLoan/HomeLoan.cs
@@ -15,6 +15,6 @@ public static class HomeLoan
 
     public static int CaculateMonthlyPayment(int borrowedAmount, int duration, decimal rate)
     {
-        return borrowedAmount;
+        return borrowedAmount / duration;
     }
 }
